3e0c6397ba7390a7c22746fd5db7cd07cd1d6069
hjl20 <88164896+hjl20@users.noreply.github.com>
2024-02-24 19:28:03 -0800

    Uncomment useful datasets

--- get-and-preprocess-dataset.sh
+++ get-and-preprocess-dataset.sh
@@ -8,12 +8,12 @@
 mkdir PSKUS_dataset
 cd PSKUS_dataset
 
-# echo "Getting the dataset..."
-# wget https://zenodo.org/record/4537209/files/DataSet1.zip?download=1 --output-document=DataSet1.zip
-# # wget https://zenodo.org/record/4537209/files/DataSet2.zip?download=1 --output-document=DataSet2.zip
-# wget https://zenodo.org/record/4537209/files/DataSet3.zip?download=1 --output-document=DataSet3.zip
-# wget https://zenodo.org/record/4537209/files/DataSet4.zip?download=1 --output-document=DataSet4.zip
-# wget https://zenodo.org/record/4537209/files/DataSet5.zip?download=1 --output-document=DataSet5.zip
+echo "Getting the dataset..."
+wget https://zenodo.org/record/4537209/files/DataSet1.zip?download=1 --output-document=DataSet1.zip
+# wget https://zenodo.org/record/4537209/files/DataSet2.zip?download=1 --output-document=DataSet2.zip
+wget https://zenodo.org/record/4537209/files/DataSet3.zip?download=1 --output-document=DataSet3.zip
+wget https://zenodo.org/record/4537209/files/DataSet4.zip?download=1 --output-document=DataSet4.zip
+wget https://zenodo.org/record/4537209/files/DataSet5.zip?download=1 --output-document=DataSet5.zip
 # wget https://zenodo.org/record/4537209/files/DataSet6.zip?download=1 --output-document=DataSet6.zip
 # wget https://zenodo.org/record/4537209/files/DataSet7.zip?download=1 --output-document=DataSet7.zip
 # wget https://zenodo.org/record/4537209/files/DataSet8.zip?download=1 --output-document=DataSet8.zip
@@ -21,12 +21,12 @@ cd PSKUS_dataset
 # wget https://zenodo.org/record/4537209/files/DataSet10.zip?download=1 --output-document=DataSet10.zip
 # wget https://zenodo.org/record/4537209/files/DataSet11.zip?download=1 --output-document=DataSet11.zip
 
-# echo "Extracting the dataset..."
-# unzip DataSet1.zip
-# # unzip DataSet2.zip
-# unzip DataSet3.zip
-# unzip DataSet4.zip
-# unzip DataSet5.zip
+echo "Extracting the dataset..."
+unzip DataSet1.zip
+# unzip DataSet2.zip
+unzip DataSet3.zip
+unzip DataSet4.zip
+unzip DataSet5.zip
 # unzip DataSet6.zip
 # unzip DataSet7.zip
 # unzip DataSet8.zip
