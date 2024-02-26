c6f4a1ddf2b1112f951378ceca8184cc5a367d28
jonathan <jparras1@my.bcit.ca>
2024-02-25 18:28:20 -0800

    Eddit the process_cvat.py to get rid rid of the 'tap on and off'

--- process_cvat_files/process_cvat.py
+++ process_cvat_files/process_cvat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import os
 
-# Function to read the single digit number from the mod directory
+# Function to read the single digit number from the pub directory
 def read_single_digit_number(filepath):
     with open(filepath) as file:
         single_digit_number = int(file.readline().strip())
@@ -10,34 +10,36 @@ def read_single_digit_number(filepath):
         single_digit_number = 0
     return single_digit_number
 
-# Process files in mod directory one by one
-def process_mod_files(mod_directory, obj_directory):
-    for mod_filename in os.listdir(mod_directory):
-        mod_filepath = os.path.join(mod_directory, mod_filename)
-        if os.path.isfile(mod_filepath):
-            # Read single digit number from the mod file
-            single_digit_number = read_single_digit_number(mod_filepath)
-            # Find matching file in obj directory
-            obj_filename = mod_filename
-            obj_filepath = os.path.join(obj_directory, obj_filename)
-            if os.path.isfile(obj_filepath) and obj_filename.endswith('.txt'):
-                # Update data in the obj file
-                update_obj_file(obj_filepath, single_digit_number)
+# Process files in pubset directory one by one
+def process_pub_files(pub_directory, cvat_directory):
+    for pub_filename in os.listdir(pub_directory):
+        pub_filepath = os.path.join(pub_directory, pub_filename)
+        if os.path.isfile(pub_filepath):
+            # Read single digit number from the pubset file
+            single_digit_number = read_single_digit_number(pub_filepath)
+            # Find matching file in cvat directory
+            cvat_filename = pub_filename
+            cvat_filepath = os.path.join(cvat_directory, cvat_filename)
+            if os.path.isfile(cvat_filepath) and cvat_filename.endswith('.txt'):
+                # Update data in the cvat file
+                update_cvat_file(cvat_filepath, single_digit_number)
 
-# Function to update data in the obj file
-def update_obj_file(obj_filepath, single_digit_number):
-    with open(obj_filepath, 'r') as file:
+# Function to update data in the cvat file
+def update_cvat_file(cvat_filepath, single_digit_number):
+    updated_lines = []
+    with open(cvat_filepath, 'r') as file:
         lines = file.readlines()
-    with open(obj_filepath, 'w') as file:
-        for line in lines:
-            parts = line.strip().split()
-            if len(parts) > 0 and float(parts[0]) == 0:
-                parts[0] = str(single_digit_number)
-            file.write(' '.join(parts) + '\n')
+    for line in lines:
+        parts = line.strip().split()
+        if len(parts) > 0 and float(parts[0]) == 0:
+            parts[0] = str(single_digit_number)
+            updated_lines.append(' '.join(parts) + '\n')
+    with open(cvat_filepath, 'w') as file:
+        file.writelines(updated_lines)
 
 # Directory paths
-mod_directory = 'mod'
-obj_directory = 'obj'
+pub_directory = 'pubset_renamed'
+cvat_directory = 'obj_train_data' # folder that contains the cvat bounding boxes txt files
 
-# Process files in mod directory and update corresponding obj files
-process_mod_files(mod_directory, obj_directory)
+# Process files in pub directory and update corresponding cvat files
+process_pub_files(pub_directory, cvat_directory)
