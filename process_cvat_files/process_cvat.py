78445b6a571a6bed4f0036a1f2f94a3d0cb53e62
hjl20 <88164896+hjl20@users.noreply.github.com>
2024-03-08 12:13:25 -0800

    Validate CVAT subfolder name to lowercase

--- process_cvat_files/process_cvat.py
+++ process_cvat_files/process_cvat.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import os
 
+# Update CVAT train folder to lowercase
+def dirname_to_lowercase(cvat_dir):
+    print(os.path.join(cvat_dir))
+    if os.path.join(cvat_dir):
+        os.rename(os.path.join(cvat_dir), os.path.join(cvat_dir).lower())
+
 # Function to read the single digit number from the pub directory
 def read_single_digit_number(filepath):
     with open(filepath) as file:
@@ -11,16 +17,16 @@ def read_single_digit_number(filepath):
     return single_digit_number
 
 # Process files in pubset directory one by one
-def process_pub_files(cvat_directory, pub_dir_txtectory):
-    for pub_filename in os.listdir(pub_dir_txtectory):
+def process_pub_files(cvat_dir, pub_dir_txt):
+    for pub_filename in os.listdir(pub_dir_txt):
         if pub_filename.endswith('.txt'):
-            pub_filepath = os.path.join(pub_dir_txtectory, pub_filename)
+            pub_filepath = os.path.join(pub_dir_txt, pub_filename)
             if os.path.isfile(pub_filepath):
                 # Read single digit number from the pubset file
                 single_digit_number = read_single_digit_number(pub_filepath)
                 # Find matching file in cvat directory
                 cvat_filename = os.path.splitext(os.path.splitext(pub_filename)[0])[0]
-                cvat_filepath = os.path.join(cvat_directory, cvat_filename + '.txt')
+                cvat_filepath = os.path.join(cvat_dir, cvat_filename + '.txt')
                 if os.path.isfile(cvat_filepath):
                     # Update data in the cvat file
                     update_cvat_file(cvat_filepath, single_digit_number)
@@ -45,8 +51,14 @@ def update_cvat_file(cvat_filepath, single_digit_number):
 def main():
     ranges = [1, 3, 4, 5]
     for i in ranges:
+
+        # Directory name validation check
         cvat_dir = f"../CVAT_dataset/CVATDataSet{i}/obj_Train_data"
+        dirname_to_lowercase(cvat_dir)
+
+        cvat_dir = f"../CVAT_dataset/CVATDataSet{i}/obj_train_data"
         pub_dir_txt = f"../PSKUS_dataset_preprocessed/DataSet{i}_TXT"
+
         process_pub_files(cvat_dir, pub_dir_txt)
         print(f"Value changes successful for set {i}!")
 
