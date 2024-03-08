0d2ab1dcdda557243844eee7436f126efae916f9
hjl20 <88164896+hjl20@users.noreply.github.com>
2024-03-08 11:48:20 -0800

    Process CVAT files on all datasets

--- process_cvat_files/filter_not_washing.py
+++ process_cvat_files/filter_not_washing.py
@@ -1,26 +1,38 @@
 #!/usr/bin/env python3
 import os
 
-data_txt_folder = "./CVATDataSet1"
-data_jpg_folder = "./DataSet1"
+def filter_not_washing(cvat_dir, pub_dir_img):
+        # Step 1: Check every single txt file in the data_txt folder
+        if cvat_dir is None or pub_dir_img is None:
+            print(f"{cvat_dir} or {pub_dir_img} folder not found. Exiting..")
+            return
 
-# Step 1: Check every single txt file in the data_txt folder
-for txt_file in os.listdir(data_txt_folder):
-    if txt_file.endswith(".txt"):
-        txt_file_path = os.path.join(data_txt_folder, txt_file)
-        
-        # Step 2: Check if the file meets the specified criteria
-        with open(txt_file_path, 'r') as file:
-            first_line = file.readline().strip()
+        for txt_file in os.listdir(cvat_dir):
+            if not txt_file.endswith(".txt"):
+                return 
+            txt_file_path = os.path.join(cvat_dir, txt_file)
             
-            if not first_line or first_line.startswith('0') or float(first_line.split()[0]) < 0:
-                # Step 3: Remove the file from data_txt and data_jpg directories
-                jpg_file = os.path.splitext(txt_file)[0] + ".jpg"
-                jpg_file_path = os.path.join(data_jpg_folder, jpg_file)
+            # Step 2: Check if the file meets the specified criteria
+            with open(txt_file_path, 'r') as file:
+                first_line = file.readline().strip()
                 
-                os.remove(txt_file_path)
-                os.remove(jpg_file_path)
-                
-                #print(f"Removed {txt_file} and {jpg_file}.")
+                if not first_line or first_line.startswith('0') or float(first_line.split()[0]) < 0:
+                    # Step 3: Remove the file from data_txt and data_jpg directories
+                    jpg_file = os.path.splitext(txt_file)[0] + ".jpg"
+                    jpg_file_path = os.path.join(pub_dir_img, jpg_file)
+                    
+                    os.remove(txt_file_path)
+                    os.remove(jpg_file_path)
+                    
 
-print("Script execution completed.")
+def main():
+    ranges = [1, 3, 4, 5]
+
+    for i in ranges:
+        cvat_dir = f"../CVAT_dataset/CVATDataSet{i}/obj_Train_data"
+        pub_dir_img = f"../PSKUS_dataset_preprocessed/DataSet{i}_IMG"
+        filter_not_washing(cvat_dir, pub_dir_img)
+        print(f"Filtering non-washing successful for set {i}!")
+
+
+main()
--- process_cvat_files/rename_txt_files.py
+++ process_cvat_files/rename_txt_files.py
@@ -1,46 +1,50 @@
 #!/usr/bin/env python3
 import os
 
-CVAT_DIR = "./CVATDataSet1"
-PUB_DIR = "./DataSet1_txt"
+def rename_txt_files(cvat_dir, pub_dir_txt):
+        # Check if directories exist
+        if not os.path.isdir(cvat_dir) or not os.path.isdir(pub_dir_txt):
+            print(f"Error: One or both of the directories ({cvat_dir} and {pub_dir_txt}) not found.")
+            exit(1)
 
-# Check if directories exist
-if not os.path.isdir(CVAT_DIR) or not os.path.isdir(PUB_DIR):
-    print(f"Error: One or both of the directories ({CVAT_DIR} and {PUB_DIR}) not found.")
-    exit(1)
+        # Function to get a list of txt files in a directory and sort them
+        def get_sorted_txt_files(directory):
+            txt_files = [entry.name for entry in os.scandir(directory) if entry.is_file() and entry.name.endswith('.txt')]
+            return sorted(txt_files)
 
-# Function to get a list of txt files in a directory and sort them
-def get_sorted_txt_files(directory):
-    txt_files = [entry.name for entry in os.scandir(directory) if entry.is_file() and entry.name.endswith('.txt')]
-    return sorted(txt_files)
+        # Function to custom sort PUB files based on timestamp and frame number
+        def custom_sort_pub_files(file_list):
+            return sorted(file_list, key=lambda x: (x.split('_frame_')[0], int(x.split('_frame_')[-1].split('.')[0])))
 
-# Function to custom sort PUB files based on timestamp and frame number
-def custom_sort_pub_files(file_list):
-    return sorted(file_list, key=lambda x: (x.split('_frame_')[0], int(x.split('_frame_')[-1].split('.')[0])))
+        # Get sorted lists of txt files for CVAT and all files (regardless of type) for PUB directory
+        cvat_files = get_sorted_txt_files(cvat_dir)
+        pub_files = custom_sort_pub_files([entry.name for entry in os.scandir(pub_dir_txt) if entry.is_file()])
 
-# Get sorted lists of txt files for CVAT and all files (regardless of type) for PUB directory
-cvat_files = get_sorted_txt_files(CVAT_DIR)
-pub_files = custom_sort_pub_files([entry.name for entry in os.scandir(PUB_DIR) if entry.is_file()])
+        # Determine the number of files to process (minimum of CVAT and PUB file counts)
+        num_files_to_process = min(len(cvat_files), len(pub_files))
 
-# Determine the number of files to process (minimum of CVAT and PUB file counts)
-num_files_to_process = min(len(cvat_files), len(pub_files))
+        # Iterate through the selected number of files and rename them
+        for i in range(num_files_to_process):
+            cvat_file = cvat_files[i]
+            pub_file = pub_files[i]
 
-# Display the first 15 elements in the sorted lists
-#print("First 15 elements in sorted cvat_files:", cvat_files[:15])
-#print("First 15 elements in sorted pub_files:", pub_files[:15])
+            # Create new file name with the same extension as the PUB file
+            pub_file_parts = pub_file.split('.')
+            extension = pub_file_parts[-1]
+            new_cvat_file_name = os.path.join(cvat_dir, pub_file_parts[0] + '.' + extension)
 
-# Iterate through the selected number of files and rename them
-for i in range(num_files_to_process):
-    cvat_file = cvat_files[i]
-    pub_file = pub_files[i]
+            # Rename the file
+            cvat_file_path = os.path.join(cvat_dir, cvat_file)
+            os.rename(cvat_file_path, new_cvat_file_name)
 
-    # Create new file name with the same extension as the PUB file
-    pub_file_parts = pub_file.split('.')
-    extension = pub_file_parts[-1]
-    new_cvat_file_name = os.path.join(CVAT_DIR, pub_file_parts[0] + '.' + extension)
 
-    # Rename the file
-    cvat_file_path = os.path.join(CVAT_DIR, cvat_file)
-    os.rename(cvat_file_path, new_cvat_file_name)
+def main():
+    ranges = [1, 3, 4, 5]
 
-print("Script executed successfully!")
+    for i in ranges:
+        cvat_dir = f"../CVAT_dataset/CVATDataSet{i}/obj_Train_data"
+        pub_dir_txt = f"../PSKUS_dataset_preprocessed/DataSet{i}_TXT"
+        rename_txt_files(cvat_dir, pub_dir_txt)
+        print(f"Renamed files successfully for set {i}!")
+
+main()
