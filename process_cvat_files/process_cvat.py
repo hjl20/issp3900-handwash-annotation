807e21485a2358e21b53d0f6611e0a0a32d4d1e8
hjl20 <88164896+hjl20@users.noreply.github.com>
2024-03-08 19:21:49 -0800

    Rename functions/variables and add file validation msgs

--- process_cvat_files/process_cvat.py
+++ process_cvat_files/process_cvat.py
@@ -1,54 +1,65 @@
 #!/usr/bin/env python3
 import os
 
-# Update CVAT train folder to lowercase
+
 def dirname_to_lowercase(cvat_dir):
-    # print(os.path.join(cvat_dir))
     if os.path.isdir(cvat_dir):
         os.rename(os.path.join(cvat_dir), os.path.join(cvat_dir).lower())
 
-# Function to read the single digit number from the pub directory
-def read_single_digit_number(filepath):
+
+def get_gesture_val(filepath):
     with open(filepath) as file:
-        single_digit_number = int(file.readline().strip())
-    # Change -1 to 0
-    if single_digit_number == -1:
-        single_digit_number = 0
-    return single_digit_number
+        gesture_val = int(file.readline().strip())
+    # Set -1 vals to be non-washing
+    if gesture_val == -1:
+        gesture_val = 0
+    return gesture_val
 
-# Process files in pubset directory one by one
-def process_pub_files(cvat_dir, pub_dir_txt):
-    if not os.path.isdir(cvat_dir) or not os.path.isdir(pub_dir_txt):
-        print(f"Error: One or both of the directories ({cvat_dir} and {pub_dir_txt}) not found.")
-        return
-    for pub_filename in os.listdir(pub_dir_txt):
-        if pub_filename.endswith('.txt'):
-            pub_filepath = os.path.join(pub_dir_txt, pub_filename)
-            if os.path.isfile(pub_filepath):
-                # Read single digit number from the pubset file
-                single_digit_number = read_single_digit_number(pub_filepath)
-                # Find matching file in cvat directory
-                cvat_filename = os.path.splitext(os.path.splitext(pub_filename)[0])[0]
-                cvat_filepath = os.path.join(cvat_dir, cvat_filename + '.txt')
-                if os.path.isfile(cvat_filepath):
-                    # Update data in the cvat file
-                    update_cvat_file(cvat_filepath, single_digit_number)
-                else:
-                    print('file not found')
-    print(f"Value changes successful for {cvat_dir}!")
 
-# Function to update data in the cvat file
-def update_cvat_file(cvat_filepath, single_digit_number):
+def update_cvat_file(cvat_filepath, gesture_val):
     updated_lines = []
     with open(cvat_filepath, 'r') as file:
         lines = file.readlines()
+
     for line in lines:
-        parts = line.strip().split()
-        if len(parts) > 0 and float(parts[0]) == 0:
-            parts[0] = str(single_digit_number)
-            updated_lines.append(' '.join(parts) + '\n')
+        label_line = line.strip().split()
+        # Only keep washing vals and convert to gesture val
+        if len(label_line) > 0 and float(label_line[0]) == 0:
+            label_line[0] = str(gesture_val)
+            updated_lines.append(' '.join(label_line) + '\n')
+
+    # Overwrite file with updated lines
     with open(cvat_filepath, 'w') as file:
         file.writelines(updated_lines)
+
+
+def process_cvat_files(cvat_dir, pub_dir_txt):
+    if not os.path.isdir(cvat_dir) or not os.path.isdir(pub_dir_txt):
+        print(f"Error: One or both of the directories ({cvat_dir} and {pub_dir_txt}) not found.")
+        return
+    
+    for pub_filename in os.listdir(pub_dir_txt):
+        if not pub_filename.endswith('.txt'):
+            print(f"{pub_filename} is not a txt. Continuing..")
+            continue
+
+        pub_filepath = os.path.join(pub_dir_txt, pub_filename)
+        
+        if not os.path.isfile(pub_filepath):
+            print(f"{pub_filepath} does not exist. Continuing..")
+            continue
+
+        gesture_val = get_gesture_val(pub_filepath)
+
+        # Find matching file in cvat directory
+        cvat_filename = os.path.splitext(os.path.splitext(pub_filename)[0])[0]
+        cvat_filepath = os.path.join(cvat_dir, cvat_filename + '.txt')
+
+        if os.path.isfile(cvat_filepath):
+            update_cvat_file(cvat_filepath, gesture_val)
+        else:
+            print(f'{cvat_filepath} not found.')
+    print(f"Value changes successful for {cvat_dir}!")
 
 
 # Process files in pub directory and update corresponding cvat files
@@ -63,8 +74,7 @@ def main():
         cvat_dir = f"../CVAT_dataset/CVATDataSet{i}/obj_train_data"
         pub_dir_txt = f"../PSKUS_dataset_preprocessed/DataSet{i}_TXT"
 
-        process_pub_files(cvat_dir, pub_dir_txt)
-
+        process_cvat_files(cvat_dir, pub_dir_txt)
 
 
 main()
