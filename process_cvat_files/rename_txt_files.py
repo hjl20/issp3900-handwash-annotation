c92194afa814e295d13e51709cf0178ff6888fcc
hjl20 <88164896+hjl20@users.noreply.github.com>
2024-03-08 19:20:48 -0800

    Refactor sort function and rename variables

--- process_cvat_files/rename_txt_files.py
+++ process_cvat_files/rename_txt_files.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python3
 import os
 
+
+def get_sorted_files(directory, ext=None):
+    if ext:
+        files = [entry.name for entry in os.scandir(directory) if entry.is_file() and entry.name.endswith(ext)]
+    else:
+        files = [entry.name for entry in os.scandir(directory) if entry.is_file()]
+    return sorted(files)
+
+
+# Function to custom sort PUB files based on timestamp and frame number
+def sort_by_frame_timestamp(pub_file_list):
+    return sorted(pub_file_list, key=lambda x: (x.split('_frame_')[0], int(x.split('_frame_')[-1].split('.')[0])))
+
+
 def rename_txt_files(cvat_dir, pub_dir_txt):
     # Check if directories exist
     if not os.path.isdir(cvat_dir) or not os.path.isdir(pub_dir_txt):
         print(f"Error: One or both of the directories ({cvat_dir} and {pub_dir_txt}) not found.")
         return
 
-    # Function to get a list of txt files in a directory and sort them
-    def get_sorted_txt_files(directory):
-        txt_files = [entry.name for entry in os.scandir(directory) if entry.is_file() and entry.name.endswith('.txt')]
-        return sorted(txt_files)
+    # Get sorted lists of txt files for CVAT and PUB directory
+    cvat_files = get_sorted_files(cvat_dir, '.txt')
+    pub_files = sort_by_frame_timestamp(get_sorted_files(pub_dir_txt, '.txt'))
 
-    # Function to custom sort PUB files based on timestamp and frame number
-    def custom_sort_pub_files(file_list):
-        return sorted(file_list, key=lambda x: (x.split('_frame_')[0], int(x.split('_frame_')[-1].split('.')[0])))
-
-    # Get sorted lists of txt files for CVAT and all files (regardless of type) for PUB directory
-    cvat_files = get_sorted_txt_files(cvat_dir)
-    pub_files = custom_sort_pub_files([entry.name for entry in os.scandir(pub_dir_txt) if entry.is_file()])
-
-    # Determine the number of files to process (minimum of CVAT and PUB file counts)
-    num_files_to_process = min(len(cvat_files), len(pub_files))
-
-    # Iterate through the selected number of files and rename them
-    for i in range(num_files_to_process):
+    # Iterate as many files as are in both sets
+    for i in range(min(len(cvat_files), len(pub_files))):
         cvat_file = cvat_files[i]
         pub_file = pub_files[i]
 
@@ -34,10 +36,12 @@ def rename_txt_files(cvat_dir, pub_dir_txt):
         new_cvat_file_name = os.path.join(cvat_dir, pub_file_parts[0] + '.' + extension)
 
         # Rename the file
-        cvat_file_path = os.path.join(cvat_dir, cvat_file)
-        os.rename(cvat_file_path, new_cvat_file_name)
+        curr_cvat_file_path = os.path.join(cvat_dir, cvat_file)
+        os.rename(curr_cvat_file_path, new_cvat_file_name)
 
     print(f"Renamed files successfully for set {cvat_dir}!")
+
+
 def main():
     ranges = [1, 3, 4, 5]
 
