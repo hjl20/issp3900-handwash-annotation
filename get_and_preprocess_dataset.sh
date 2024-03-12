f1af63b69ca9f4b5b1ccd65c6c3f04628764a761
jonathan <jparras1@my.bcit.ca>
2024-03-11 19:10:44 -0700

    uncomment set1 and 5 in sh

--- get_and_preprocess_dataset.sh
+++ get_and_preprocess_dataset.sh
@@ -9,11 +9,11 @@ mkdir PSKUS_dataset
 cd PSKUS_dataset
 
 echo "Getting the dataset..."
-# wget https://zenodo.org/record/4537209/files/DataSet1.zip?download=1 --output-document=DataSet1.zip
+wget https://zenodo.org/record/4537209/files/DataSet1.zip?download=1 --output-document=DataSet1.zip
 # wget https://zenodo.org/record/4537209/files/DataSet2.zip?download=1 --output-document=DataSet2.zip
 wget https://zenodo.org/record/4537209/files/DataSet3.zip?download=1 --output-document=DataSet3.zip
 wget https://zenodo.org/record/4537209/files/DataSet4.zip?download=1 --output-document=DataSet4.zip
-# wget https://zenodo.org/record/4537209/files/DataSet5.zip?download=1 --output-document=DataSet5.zip
+wget https://zenodo.org/record/4537209/files/DataSet5.zip?download=1 --output-document=DataSet5.zip
 # wget https://zenodo.org/record/4537209/files/DataSet6.zip?download=1 --output-document=DataSet6.zip
 # wget https://zenodo.org/record/4537209/files/DataSet7.zip?download=1 --output-document=DataSet7.zip
 # wget https://zenodo.org/record/4537209/files/DataSet8.zip?download=1 --output-document=DataSet8.zip
@@ -22,11 +22,11 @@ wget https://zenodo.org/record/4537209/files/DataSet4.zip?download=1 --output-do
 # wget https://zenodo.org/record/4537209/files/DataSet11.zip?download=1 --output-document=DataSet11.zip
 
 echo "Extracting the dataset..."
-# unzip DataSet1.zip
+unzip DataSet1.zip
 # unzip DataSet2.zip
 unzip DataSet3.zip
 unzip DataSet4.zip
-# unzip DataSet5.zip
+unzip DataSet5.zip
 # unzip DataSet6.zip
 # unzip DataSet7.zip
 # unzip DataSet8.zip
